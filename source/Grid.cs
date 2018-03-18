a374d91eb07429ba64577dfc60d79d6d08a59011
emiel <emielvdbrink@gmail.com>
2018-03-18 23:33:21 +0100

    added some of the original source files

---
+++ source/Grid.cs
@@ -0,0 +1,135 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using UnityEngine.SceneManagement;
+
+
+public class Grid : MonoBehaviour
+{
+    public GameObject tileSelector;
+    public Dropdown tileSelectorScript;
+
+    public GameObject tile;
+    public GameObject[,] grid;
+    public Vector2 fieldsize;
+
+    public bool isPlaying;
+    public bool calculating;
+
+    public Camera mainCam;
+    public Vector3 lastPos;
+    public Vector3 targetPos;
+    public float startTime;
+
+    public float nextCalcTime;
+    public GameObject timeSlider;
+    public Slider timeSliderScript;
+
+    void Start ()
+    {
+        tileSelector        = GameObject.Find("TileMenu");
+        tileSelectorScript  = tileSelector.GetComponent<Dropdown>();
+        timeSlider          = GameObject.Find("TimeSlider");
+        timeSliderScript    = timeSlider.GetComponent<Slider>();
+
+        GridSize();
+
+        calculating = true;
+        GenerateGrid();
+        SetCam();
+	}
+	
+	void Update ()
+    {
+        SetCam();
+
+        //makes sure that every tile runs the CheckTile command if the grid is in calculating state
+        if (Time.time <= nextCalcTime) return;
+        nextCalcTime = Time.time + timeSliderScript.value;
+
+        if (!isPlaying || grid[0, 0] == null) return;
+
+        for (int x = 0; x < fieldsize.x; x++) for (int y = 0; y < fieldsize.y; y++)
+        {
+            if (calculating) grid[x, y].GetComponent<Tile>().CheckTile();
+            else             grid[x, y].GetComponent<Tile>().UpdateTile();
+        }
+        calculating = !calculating;
+    }
+
+    //creates a new gameobject tile for every space in the array
+    public void GenerateGrid()
+    {
+        grid = new GameObject[Mathf.CeilToInt(fieldsize.x), Mathf.CeilToInt(fieldsize.y)];
+        Vector3 pos = new Vector3(0, 0, 0);
+        for (int x = 0; x < fieldsize.x; x++)
+            for (int y = 0; y < fieldsize.y; y++)
+            {
+                pos.x = x;
+                pos.y = y;
+                GameObject newTile = (GameObject)Instantiate(tile, new Vector3(pos.x, pos.y, 0), Quaternion.identity);
+                newTile.name = x + "," + y;
+                newTile.GetComponent<Tile>().myPos = pos;
+                newTile.GetComponent<Tile>().gm = GetComponent<Grid>();
+                newTile.tag = "Tile";
+                grid[x, y]  = newTile;
+            }
+    }
+
+    //clears all the gameobject that are stored inside the grid
+    public void DeleteGrid()
+    {
+        for (int x = 0; x < fieldsize.x; x++)
+            for (int y = 0; y < fieldsize.y; y++)
+                Destroy(grid[x, y]);
+    }
+
+    //switches between starting and stopping
+    public void SwitchPlaying()
+    {
+        isPlaying   = !isPlaying;
+        calculating = true;
+    }
+
+    //issues the ClearTiles command for all tiles in the array
+    public void ClearTiles()
+    {
+        for (int x = 0; x < fieldsize.x; x++)
+            for (int y = 0; y < fieldsize.y; y++)
+                grid[x, y].GetComponent<Tile>().ClearTile();
+    }
+
+    //sets the gridsize according to the filled in value
+    public void GridSize()
+    {
+        switch (tileSelectorScript.value)
+        {
+            case 0: fieldsize = new Vector2(10, 10); break;
+            case 1: fieldsize = new Vector2(20, 20); break;
+            case 2: fieldsize = new Vector2(30, 30); break;
+            case 3: fieldsize = new Vector2(40, 40); break;
+        }
+
+        startTime = Time.time;
+        lastPos   = mainCam.transform.position;
+        targetPos = new Vector3(fieldsize.x / 2, fieldsize.y / 2, -fieldsize.x);
+    }
+
+    public void SetCam()
+    {       
+        mainCam.transform.position = Vector3.Lerp(lastPos, targetPos, 0.5f * (Time.time - startTime));
+    }
+
+    public void StopPlaying()
+    {
+        isPlaying = false;
+    }
+
+    public void Quit()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+}
---
+++ source/Tile.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour {
+    public bool isAlive;
+    public MeshRenderer rnd;
+
+    public Vector2 myPos;
+    public Grid gm;
+
+    public int aliveNBTiles;
+	// Use this for initialization
+	void Start ()
+    {
+        rnd = GetComponent<MeshRenderer>();
+	}
+
+	//checks wether the state of the tile should be alive or dead according to its surrounding neighbours
+    public void CheckTile()
+    {
+        aliveNBTiles = 0;
+        for (int x = -1; x <= 1; x++)
+        {
+            for (int y = -1; y <= 1; y++)
+            {
+                if (x == 0 && y == 0) continue; //don't check myself
+                int nbx = Mathf.CeilToInt(myPos.x + x);
+                int nby = Mathf.CeilToInt(myPos.y + y);
+                if (nbx >= 0 && nby >= 0 && nbx < gm.grid.GetLength(0) && nby < gm.grid.GetLength(1))
+                {
+                   if (gm.grid[nbx, nby].GetComponent<Tile>().isAlive) aliveNBTiles++;
+                }
+            }
+        }
+    }
+
+    //updates the tile to the desired state decided in the Checktile command
+    public void UpdateTile()
+    {
+        if      (aliveNBTiles == 3) isAlive = true;
+        else if (aliveNBTiles <  2) isAlive = false;
+        else if (aliveNBTiles >  3) isAlive = false;
+
+        rnd.material.color = (isAlive ? Color.black : Color.white);
+    }
+
+    //sets the tile to the "dead" state
+    public void ClearTile()
+    {
+        if (isAlive) rnd.material.color = Color.white;
+        isAlive = false;
+    }
+
+    //sets the state to "alive" when there's input from the left mousebutton, "dead" if there's input from the right
+    private void OnMouseOver()
+    {
+        if (Input.GetKey(KeyCode.Mouse0)) { isAlive = true;  rnd.material.color = Color.black; }
+        if (Input.GetKey(KeyCode.Mouse1)) { isAlive = false; rnd.material.color = Color.white; }
+    }
+}
